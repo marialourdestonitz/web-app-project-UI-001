140ae74e8ad649e4a5bdfc743e1d3169ba40e6f6
marialourdestonitz <marialourdestonitz@outlook.com>
2024-07-10 11:03:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const triggerOpen = document.querySelectorAll('[trigger-button]');
+const triggerClose = document.querySelectorAll('[close-button]');
+const overlay = document.querySelector('[data-overlay]');
+
+for (let i = 0; i < triggerOpen.length; i++) {
+    let currentId = triggerOpen[i].dataset.target,
+        targetEl = document.querySelector(`#${currentId}`)
+
+    const openData = function () {
+        targetEl.classList.remove('active');
+        overlay.classList.remove('active');
+    };
+
+    triggerOpen[i].addEventListener('click', function () {
+        targetEl.classList.add('active');
+        overlay.classList.add('active');
+    });
+
+    targetEl.querySelector('[close-button]').addEventListener('click', openData);
+    overlay.addEventListener('click', openData);
+};
+
+/*MOBILE*/
+const submenu = document.querySelectorAll('.child-trigger');
+submenu.forEach((menu) => menu.addEventListener('click', function (e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('active') : null);
+    this.closest('.has-child').classList.toggle('active');
+}));
+
+
